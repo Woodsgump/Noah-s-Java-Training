4bf5de21444c590b5f4980c04f59cc9e43074854
Noah Cavazos <97990650+Woodsgump@users.noreply.github.com>
2022-06-23 08:43:09 -0500

    Add files via upload
    Updating  the Pet classes

--- pet-app/src/dev/cavazos/petapp/main/Pet.java
+++ pet-app/src/dev/cavazos/petapp/main/Pet.java
@@ -1,20 +1,26 @@
 package dev.cavazos.petapp.main;
 
+import java.io.Serializable;
+import java.util.Objects;
 import java.util.Random;
 
-public class Pet {
+// fully encapsulated class: private fields + public getter/setter methods
+
+// "java bean" fully encapsulated class w/ a no-args constructor
+// & overridden Object methods (hashCode, equals, toString), implements Serializable
+
+public class Pet implements Serializable {
 	// class members: states/behaviors or characteristics/actions in a class
 	// (fields and methods)
-	public int id;
-	public String name;
-	public int age;
-	public String species;
+	private int id;
+	private String name;
+	private int age;
+	private String species;
 	private String secret;
 	
 	// static: belongs to the class itself rather than individual objects
 	// all of the instances (objects) share it
 	public static int count = 0;
-	
 	public static boolean randomBoolean;
 	
 	// instance block: called every time you instantiate an object no matter
@@ -76,9 +82,91 @@ public class Pet {
 		System.out.println(this.name + " ate the " + food);
 	}
 	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (this.age == age) {} // primitives can use == 
+		if (this.species.equals("")) {} // objects use .equals()
+		//this.age.equals(8) // primitives aren't objects so you can't call methods
+		
+		if(age >= 0) {
+			switch(this.species) {
+			case "dog":
+					age = age * 7;
+					break;
+			case "cat":
+					age = age * 3;
+					break;
+			}
+			this.age = age;
+		} else {
+			this.age = 0;
+		}
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public String getSecret() {
+		return secret;
+	}
+
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+
 	// static method: you can call it without having and object/instance
 	// of this class
 	static void printPetCount() {
 		System.out.println(count);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, id, name, secret, species);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pet other = (Pet) obj;
+		return age == other.age && id == other.id && Objects.equals(name, other.name)
+				&& Objects.equals(secret, other.secret) && Objects.equals(species, other.species);
+	}
+
+	@Override
+	public String toString() {
+		return "Pet [id=" + id + ", name=" + name + ", age=" + age + ", species=" + species + ", secret=" + secret
+				+ "]";
 	}
 }
--- pet-app/src/dev/cavazos/petapp/main/PetAppMain.java
+++ pet-app/src/dev/cavazos/petapp/main/PetAppMain.java
@@ -1,6 +1,12 @@
 package dev.cavazos.petapp.main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
+
+import dev.cavazos.petapp.data.FileAccessObject;
 
 // classes are blueprints for objects
 public class PetAppMain {
@@ -8,13 +14,93 @@ public class PetAppMain {
 		// protected: same package & subclasses
 		// "default" (no modifier): same package only
 		// private: only within the same class
-	
-	public static void main(String[] args) {
+		static Scanner scanner = new Scanner(System.in);
+		
+		public static void main(String[] args) {
 			// petClassObjectExample();
-			controlFlow();
-	}
-	
-	private static void controlFlow() {
+			//controlFlow();
+			objectClass();
+			
+			//Object obj = null;
+			//if (obj != null)
+			//	obj.hashCode();
+			
+			petApp();
+			
+			scanner.close();
+		}
+		
+		private static void petApp(){
+			Pet[] pets = new Pet[3];
+			FileAccessObject fileAccessObject = new FileAccessObject();
+			
+			// handling exceptions right now
+			try {
+					File file = new File("pets.dat");
+					fileAccessObject.readObjects(file);
+			} catch (FileNotFoundException e) {
+				// this is whatever we want to happen if the exception does not get thrown
+				e.printStackTrace();
+			} catch (Exception e) {
+				// this will handle any exceptions that aren't caught by the above
+				// catch block(s)
+				e.printStackTrace();
+			} finally {
+				// finally blocks are optional (when you have a try you either have to
+				// have at least one catch or finally)
+				System.out.println("This always happens either way");
+			}
+			
+			boolean userContinue = true;
+			
+			// condition must be true
+			while (userContinue) {
+				System.out.println("Type 1 to add a pet,"
+								+ "type 2 to view pets, type anything to exit");
+				String input = scanner.nextLine();
+				
+				switch (input) {
+				case "1":
+						// add a pet
+						System.out.println("Enter a pet name: ");
+						String name = scanner.nextLine();
+						System.out.println("Enter the pet's age: ");
+						String ageInput = scanner.nextLine(); // why is the variable a string instead of an input???
+						
+						int age = Integer.parseInt(scanner.nextLine());
+						System.out.println("Enter the pet's species: ");
+						String species = scanner.nextLine();
+						System.out.println("Enter the pet's secret: ");
+						String secret = scanner.nextLine();
+						
+						Pet newPet = new Pet(name, age, species, secret);
+						int index = newPet.getId() - 1;
+						
+						pets[index] = newPet;
+						break;
+				case "2":
+						// view pets
+						System.out.println(Arrays.toString(pets));
+						break;
+				default:
+						userContinue = false;
+						break;
+				}
+			}
+		}
+		
+		private static void objectClass() {
+			Pet myPet = new Pet();
+			myPet.setId(1);
+			Pet otherPet = new Pet();
+			otherPet.setId(1);
+			
+			System.out.println(myPet.hashCode());
+			System.out.println(myPet.equals(otherPet));
+			System.out.println(myPet); //(myPet.toString()) implicitly
+		}
+		
+		private static void controlFlow() {
 			// conditional statements
 			// if-else
 			// switch
@@ -93,7 +179,7 @@ public class PetAppMain {
 			}
 	}
 	
-	private static void petClassObjectExample() {
+	public static void petClassObjectExample() {
 		// create an object using the class as a blueprint
 		
 		Pet.printPetCount();
@@ -102,14 +188,15 @@ public class PetAppMain {
 		// declare a variable, assigning it a value
 		// instantiating (creating) an object by calling a constructor
 		Pet myPet = new Pet();
-		myPet.name = "Guap";
-		System.out.println("pet id: " + myPet.id);
+		//myPet.name = "Guap";
+		myPet.setName("Guap");
+		System.out.println("pet id: " + myPet.getId());
 		// myPet.secret = "";
 		// type of variable: the class
 		
 		Pet myLizard = new Pet("Spyro", 4, "lizard", "breathes flames");
 		myLizard.eat("bug");
-		System.out.println("pet id: " + myLizard.id);
+		System.out.println("pet id: " + myLizard.getId());
 		System.out.println(myLizard.randomBoolean);
 		
 		Pet.printPetCount();
